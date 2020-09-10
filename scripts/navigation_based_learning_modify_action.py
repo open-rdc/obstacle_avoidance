3cb605fc48349879c40c58aab73aeca4b30b41c5
MasayaOkada <masa.okd888@gmail.com>
2020-09-10 15:43:27 +0900

    init

---
+++ scripts/navigation_based_learning_modify_action.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+roslib.load_manifest('obstacle_avoidance')
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import LaserScan
+from kobuki_msgs.msg import BumperEvent
+from cv_bridge import CvBridge, CvBridgeError
+from deep_learning import *
+from skimage.transform import resize
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_msgs.msg import Float32, Int8
+from nav_msgs.msg import Path
+from std_srvs.srv import Trigger
+from actionlib_msgs.msg import GoalStatusArray
+from std_srvs.srv import Empty
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.srv import GetModelState
+from gazebo_msgs.msg import ModelState
+from std_srvs.srv import SetBool, SetBoolResponse
+import csv
+import os
+import time
+import copy
+import random
+import math
+import sys
+
+class cource_following_learning_node:
+	def __init__(self):
+		rospy.init_node('cource_following_learning_node', anonymous=True)
+		self.action_num = rospy.get_param("/LiDAR_based_learning_node/action_num", 1)
+		print("action_num: " + str(self.action_num))
+		self.dl = deep_learning(n_action = self.action_num)
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+		self.image_left_sub = rospy.Subscriber("/camera_left/rgb/image_raw", Image, self.callback_left_camera)
+		self.image_right_sub = rospy.Subscriber("/camera_right/rgb/image_raw", Image, self.callback_right_camera)
+#		self.bumper_sub = rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, self.callback_bumper)
+		self.vel_sub = rospy.Subscriber("/nav_vel", Twist, self.callback_vel)
+		self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.callback_scan)
+		self.amcl_pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.callback_amcl_pose)
+		self.nav_path_sub = rospy.Subscriber("/move_base/DWAPlannerROS/local_plan", Path, self.callback_nav_path)
+		self.action_pub = rospy.Publisher("action", Int8, queue_size=1)
+		self.nav_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+		self.srv = rospy.Service('/training', SetBool, self.callback_dl_training)
+		self.action = 0.0
+		self.reward = 0
+		self.episode = 0
+		self.count = 0
+		self.status = 0
+		self.amcl_pose = 0
+		self.nav_path = 0
+		self.loop_count = 0
+		self.success = 0.0
+		self.vel = Twist()
+		self.cv_image = np.zeros((480,640,3), np.uint8)
+		self.cv_left_image = np.zeros((480,640,3), np.uint8)
+		self.cv_right_image = np.zeros((480,640,3), np.uint8)
+		self.learning = True
+		self.collision = False
+		self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
+		self.action_list = ['Front', 'Right', 'Left']
+		self.path = 'data/result'
+		self.previous_reset_time = 0
+		self.start_time_s = rospy.get_time()
+		self.select_dl_out = False
+		self.correct_count = 0
+		self.incorrect_count = 0
+		self.distance = 0
+		os.makedirs(self.path + self.start_time)
+
+		with open(self.path + self.start_time + '/' +  'reward.csv', 'w') as f:
+			writer = csv.writer(f, lineterminator='\n')
+			writer.writerow(['epsode', 'time(s)', 'reward'])
+
+	def callback(self, data):
+		try:
+			self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+	def callback_left_camera(self, data):
+		try:
+			self.cv_left_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+	def callback_right_camera(self, data):
+		try:
+			self.cv_right_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+	def callback_scan(self, scan):
+		points = []
+		angle = scan.angle_min
+		for distance in scan.ranges:
+			if distance != float('inf') and not math.isnan(distance):
+				points.append((distance * math.cos(angle), distance * math.sin(angle)))
+			angle += scan.angle_increment
+
+			if distance <= 0.2:
+				self.collision = True
+
+	def callback_vel(self, data):
+		self.vel = data
+# action
+		self.action = self.vel.angular.z
+
+	def callback_amcl_pose(self, data):
+		self.amcl_pose = data
+
+	def callback_nav_path(self, data):
+		self.nav_path = data
+
+	def callback_modify_action(self):
+		diff_x = self.amcl_pose.pose.pose.position.x - self.nav_path.poses[0].pose.position.x
+		diff_y = self.amcl_pose.pose.pose.position.xy - self.nav_path.poses[0].pose.position.y
+		self.distance = math.sqrt(diff_x**2 + diff_y**2)
+		if self.distance > 0.15:
+			self.select_dl_out = True
+		elif self.distance < 0.05:
+			self.select_dl_out = False
+
+	def callback_dl_training(self, data):
+		resp = SetBoolResponse()
+		self.learning = data.data
+		resp.message = "Training: " + str(self.learning)
+		resp.success = True
+		return resp
+
+	def loop(self):
+		if self.cv_image.size != 640 * 480 * 3:
+			return
+		if self.cv_left_image.size != 640 * 480 * 3:
+			return
+		if self.cv_right_image.size != 640 * 480 * 3:
+			return
+
+		rospy.wait_for_service('/gazebo/get_model_state')
+		get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+		try:
+			previous_model_state = get_model_state('mobile_base', 'world')
+		except rospy.ServiceException as exc:
+			print("Service did not process request: " + str(exc))
+		if self.vel.linear.x == 0:
+			return
+		img = resize(self.cv_image, (48, 64), mode='constant')
+		r, g, b = cv2.split(img)
+		imgobj = np.asanyarray([r,g,b])
+
+		img_left = resize(self.cv_left_image, (48, 64), mode='constant')
+		r, g, b = cv2.split(img_left)
+		imgobj_left = np.asanyarray([r,g,b])
+
+		img_right = resize(self.cv_right_image, (48, 64), mode='constant')
+		r, g, b = cv2.split(img_right)
+		imgobj_right = np.asanyarray([r,g,b])
+
+		ros_time = str(rospy.Time.now())
+
+		if self.learning:
+			if self.loop_count < 100:
+				self.reward = 0
+				action = self.dl.act_and_trains(imgobj, self.action)
+				if abs(action - self.action) < 0.2:
+					self.correct_count += 1
+					self.incorrect_count = 0
+				else:
+					self.incorrect_count += 1
+					self.correct_count = 0
+				if self.correct_count >= 5:
+					self.select_dl_out = True
+				if self.incorrect_count >= 2:
+					self.select_dl_out = False
+				if abs(self.action) < 0.1:
+					action_left = self.dl.act_and_trains(imgobj_left, self.action - 0.2)
+					action_right = self.dl.act_and_trains(imgobj_right, self.action + 0.2)
+				self.count += 1
+				self.loop_count += 1
+				self.success += abs(action - self.action)
+				print(self.distance)
+			else:
+				action = self.dl.act_and_trains(imgobj, self.action)
+				line = [str(self.episode), str(self.success)]
+				with open(self.path + self.start_time + '/' + 'reward.csv', 'a') as f:
+					writer = csv.writer(f, lineterminator='\n')
+					writer.writerow(line)
+				print(" episode: " + str(self.episode) + ", success ratio: " + str(self.success) + " " + str(self.count))
+				self.count = 0
+				self.success = 0.0
+				self.loop_count = 0
+				self.collision = False
+				self.episode += 1
+			if self.select_dl_out == True:
+				self.action = self.dl.act(imgobj)
+			self.vel.linear.x = 0.2
+			self.vel.angular.z = self.action
+			self.nav_pub.publish(self.vel)
+
+		else:
+			self.action = self.dl.act(imgobj)
+			print("TEST MODE: " + str(self.action))
+			print(self.distance)
+			self.vel.linear.x = 0.2
+			self.vel.angular.z = self.action
+			self.nav_pub.publish(self.vel)
+
+		temp = copy.deepcopy(img)
+		cv2.imshow("Resized Image", temp)
+		temp = copy.deepcopy(img_left)
+		cv2.imshow("Resized Left Image", temp)
+		temp = copy.deepcopy(img_right)
+		cv2.imshow("Resized Right Image", temp)
+		cv2.waitKey(1)
+
+if __name__ == '__main__':
+	rg = cource_following_learning_node()
+	DURATION = 0.2
+	r = rospy.Rate(1 / DURATION)
+	while not rospy.is_shutdown():
+		rg.loop()
+		r.sleep()
--- scripts/test.py
+++ scripts/test.py
@@ -2,27 +2,43 @@
 
 import rospy
 from actionlib_msgs.msg import GoalStatusArray
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_msgs.msg import Float32
+from nav_msgs.msg import Path
+import math
 import numpy as np
 
 class testNode:
     def __init__(self):
-        
         rospy.init_node('testNode', anonymous=True)
-        self.test_sub= rospy.Subscriber("/move_base/status", GoalStatusArray, self.callback_data)
-        self.data =0
+        self.amcl_pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.callback_amcl_pose)
+        self.nav_path_sub = rospy.Subscriber("/move_base/DWAPlannerROS/local_plan", Path, self.callback_nav_path)
+        self.distance_pub = rospy.Publisher("distance", Float32, queue_size=1)
+        self.data = 0
+        self.distance = 0
+        self.amcl_pose = 0
+        self.nav_path = 0
 
-    def callback_data(self, data):
-        self.data = data.status_list[0].status
-        count = []
-        next_count = []
-        count.append(self.data)
-        next_count.append(self.data)
+    def callback_amcl_pose(self, data):
+        self.amcl_pose = data
 
-#        split = (self.data[0],self.data[1])
-        print(count, next_count)
+    def callback_nav_path(self, data):
+        self.nav_path = data
+
+    def callback_modify_action(self):
+
+        diff_x = self.amcl_pose.pose.pose.position.x - self.nav_path.poses[0].pose.position.x
+        diff_y = self.amcl_pose.pose.pose.position.y - self.nav_path.poses[0].pose.position.y
+        self.distance = math.sqrt(diff_x ** 2 + diff_y ** 2)
+        print(self.distance)
+        self.distance_pub.publish(self.distance)
+
+#    def callback_data(self, data):
+#        self.data = data.pose.pose.position.x
+#        self.data = data.poses[0].pose.position.x
+#        print(self.data)
 #        rospy.loginfo("x:%f", GoalStatusArray)
 
 if __name__ =='__main__':
     tn = testNode()
     rospy.spin()
-
